dac333b0594041d5ee74ceb796c3f2fcf8d0d0ae
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-04-10 12:46:46 +0700

    set atribut privasi

--- src/praktek03/kulkas.java
+++ src/praktek03/kulkas.java
@@ -2,9 +2,9 @@ package praktek03;
 
 
 public class kulkas {
-    String merek ;
-    String warna ;
-    String harga ;
+   private String merek ;
+   private String warna ;
+   private String harga ;
     
    void cetakInfo(){
        System.out.println("Merek \t: "+merek +"\n"+"Warna \t: "+warna+"\n"+"Harga \t:"+harga);
