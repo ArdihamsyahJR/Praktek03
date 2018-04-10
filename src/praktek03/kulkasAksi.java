c44a5db8e3af94de3566651daa76501b41619e43
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-04-10 12:56:10 +0700

    menggunakan method set

--- src/praktek03/kulkasAksi.java
+++ src/praktek03/kulkasAksi.java
@@ -3,10 +3,10 @@ package praktek03;
 public class kulkasAksi {
     public static void main(String[] args) {
         kulkas k = new kulkas();
-        k.merek="toshiba";
-        k.warna="silver";
-        k.harga="500";
-        
+      
+        k.setMerek("buterfly");
+        k.setWarna("silver");
+        k.setHarga("500");
    k.cetakInfo();
     }
  
