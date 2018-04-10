7bd6e145ff180013ca521552fa4e28d8425f174e
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-04-10 13:13:39 +0700

    gatter and setter

--- src/praktek03/kulkas2pintu.java
+++ src/praktek03/kulkas2pintu.java
@@ -6,4 +6,14 @@ public class kulkas2pintu extends kulkas{
     void throttle(){
       System.out.println("Ademmm.........");
     }
+
+    public String getJeniskulkas() {
+        return jeniskulkas;
+    }
+
+    public void setJeniskulkas(String jeniskulkas) {
+        this.jeniskulkas = jeniskulkas;
+    }
+    
+    
 }
