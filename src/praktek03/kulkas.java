f523c5fe0933f749122d95f1f2a233403f52821d
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-04-10 12:49:25 +0700

    buat getter and setter

--- src/praktek03/kulkas.java
+++ src/praktek03/kulkas.java
@@ -9,5 +9,31 @@ public class kulkas {
    void cetakInfo(){
        System.out.println("Merek \t: "+merek +"\n"+"Warna \t: "+warna+"\n"+"Harga \t:"+harga);
    }
+
+    public String getMerek() {
+        return merek;
+    }
+
+    public void setMerek(String merek) {
+        this.merek = merek;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+   
+   
 }
 
