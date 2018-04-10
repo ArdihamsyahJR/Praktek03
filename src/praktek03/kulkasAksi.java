0777436a45fff32bc482b469894924cb45803b3b
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-04-10 13:03:27 +0700

    menggunakan method get

--- src/praktek03/kulkasAksi.java
+++ src/praktek03/kulkasAksi.java
@@ -4,10 +4,18 @@ public class kulkasAksi {
     public static void main(String[] args) {
         kulkas k = new kulkas();
       
-        k.setMerek("buterfly");
+        k.setMerek("Toshiba");
         k.setWarna("silver");
         k.setHarga("500");
    k.cetakInfo();
+ 
+   System.out.println("Mereknya \t: ");
+   System.out.println(k.getMerek());
+   System.out.println("Warnaknya \t: ");
+   System.out.println(k.getWarna());
+   System.out.println("Harganya \t: ");
+   System.out.println(k.getHarga());
+    
     }
  
 }
