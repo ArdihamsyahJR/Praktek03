ec39cac3c1d14913ff5103e8dcc5eb0b0d6541d0
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-04-10 12:44:49 +0700

    instance buat atribut dan panggil method

--- src/praktek03/kulkasAksi.java
+++ src/praktek03/kulkasAksi.java
@@ -2,7 +2,12 @@ package praktek03;
 
 public class kulkasAksi {
     public static void main(String[] args) {
+        kulkas k = new kulkas();
+        k.merek="toshiba";
+        k.warna="silver";
+        k.harga="500";
         
+   k.cetakInfo();
     }
  
 }
